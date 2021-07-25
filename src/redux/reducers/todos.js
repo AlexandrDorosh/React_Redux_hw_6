4bea3751527060495f89e749386ae20b2803aad3
AlexandrDorosh <dovgaloleksandr388@gmail.com>
2021-07-25 17:16:28 +0300

    finish hw 6

---
+++ src/redux/reducers/todos.js
@@ -0,0 +1,33 @@
+import {
+    ADD_TODOS,
+    LOADING_TRUE,
+    LOADING_FALSE,
+    PUSH_TODO,
+} from '../actionTypes'
+
+const initialState = {
+    todos: [],
+    todosLoading: false,
+}
+
+export const todosReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TODOS: {
+            return {...state, todos: action.payload}
+        }
+        case LOADING_TRUE: {
+            return {...state, todosLoading: true}
+        }
+        case LOADING_FALSE: {
+            return {...state, todosLoading: false}
+        }
+        case PUSH_TODO: {
+            return {...state, todos: [...state.todos, action.payload]}
+        }
+        case "DELETE_TODO": {
+            return {...state, todos: [...state.todos.filter(value => value.id !== action.payload.id)]}
+        }
+        default:
+            return state
+    }
+}
